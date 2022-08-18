d65a13cccae2856bc937e756fb85cd7d68499d8e
np <niepeng@88.com>
2022-08-18 11:51:04 +0800

    fix：去除 url 地址后面的参数

--- js/popup.js
+++ js/popup.js
@@ -1,14 +1,23 @@
 $(function () {
-  var slt = $('#jxapis')
-  var toast = $('#toast')
-  var loading = $('#loading')
-  var ipt = $('#ipt')
-  var timer = null
+  const slt = $('#jxapis')
+  const toast = $('#toast')
+  const loading = $('#loading')
+  const ipt = $('#ipt')
+  let timer = null
 
   initOptions()
 
+  function getUrl (url) { 
+    if (url && url.match(/^https?\:\/\//)) {
+      const {origin, pathname} = new URL(url)
+      return origin + pathname
+    } else {
+      return url
+    }
+  }
+
   function initOptions () {
-    var opts = ''
+    let opts = ''
     for (key in jxapis) {
       if (jxapis[key]) {
         opts += `<option value="${jxapis[key]}">${key}</option>`
@@ -18,12 +27,13 @@ $(function () {
   }
 
   slt.change(function (e) {
-    var iptVal = (ipt.val() || '').trim()
+    let iptVal = (ipt.val() || '').trim()
     const val = e.target.value
     if (!iptVal) {
       slt.find('option').eq(0).prop('selected', true)
       showToast('地址不能为空')
     } else {
+      iptVal = getUrl(iptVal)
       window.open(val + iptVal, 'kankan', 'width='+(window.screen.availWidth)+',height='+(window.screen.availHeight)+ ', fullscreen=yes, toolbar=no, menubar=no, scrollbars=no, resizable=no, location=no, status=no')
     }
   })
